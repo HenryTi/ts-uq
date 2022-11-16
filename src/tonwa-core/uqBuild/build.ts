1c0a0b1bfed391716cf085645baf09bbe3d5d173
HenryTi <liaohengyi@gmail.com>
2022-11-15 22:45:29 -0500

    centerHost

--- src/tonwa-core/uqBuild/build.ts
+++ src/tonwa-core/uqBuild/build.ts
@@ -44,8 +44,8 @@ export async function build(uqConfigs: UqConfig[], buildContext: UqBuildContext)
     //fs.unlinkSync(uqTsSrcPath + '/uqs.ts');
 
     //let centerHost = 'https://dev.tonwa.ca';
-    let centerHost = 'http://localhost:3000';
-    //let centerHost = 'https://tv.jkchemical.com';
+    //const centerHost = 'http://localhost:3000';
+    const centerHost = 'https://tv.jkchemical.com';
     let centerToken = undefined;
     let centerChannel = new CenterHttpChannel(buildContext.web, centerHost, centerToken);
 
